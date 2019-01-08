f62f9c2303200389c94012e71e52e557c0a1d694
pgs5 <p_g_summers@msn.com>
2019-01-08 03:12:26 +0000

    Added filtering for users in blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('about/',views.blog_about, name='blog-about'),
-    path('blog/<int:pk>/', PostDetailView.as_view(), name ='post-detail'),
-    path('blog/new', PostCreateView.as_view(), name='post-create'),
-    path('blog/<int:pk>/update/', PostUpdateView.as_view(), name ='post-update'),
-    path('blog/<int:pk>/delete/', PostDeleteView.as_view(), name ='post-delete')
+    path('<int:pk>/', PostDetailView.as_view(), name ='post-detail'),
+    path('new', PostCreateView.as_view(), name='post-create'),
+    path('<int:pk>/update/', PostUpdateView.as_view(), name ='post-update'),
+    path('<int:pk>/delete/', PostDeleteView.as_view(), name ='post-delete'),
+    path('user/<str:username>',UserPostListView.as_view(), name = 'user-posts')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.contrib.auth.models import User
 
 def blog_about(request):
     return render(request, 'blog/about.html', {'title':"About"})
@@ -12,7 +13,6 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
     paginate_by = 3
-
 
 class PostDetailView(DetailView):
     model = Post
@@ -48,3 +48,12 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         if self.request.user == post.author:
             return True
         return False
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_post.html'
+    paginate_by = 3
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username =self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
