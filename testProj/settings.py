977a37a524e8ad71bc21a3817c8cf85f68bbbc8c
pgs5 <p_g_summers@msn.com>
2019-01-12 15:58:45 +0000

    Removed Key

--- testProj/settings.py
+++ testProj/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
-import os
+import os, json
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,13 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zzt4qkay4qe8p82!)6*1$0(+pq#832kss(h8%k0^hgr6x$hc%p'
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['192.168.1.199']
 
 # Application definition
 
